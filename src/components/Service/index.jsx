68b84478f361aca76f683ebee7cc3bead44b9f98
Imre Kiss <imre.kiss@jaystack.com>
2022-06-24 17:36:07 +0200

    Fix build errors

--- src/components/Service/index.jsx
+++ src/components/Service/index.jsx
@@ -6,11 +6,7 @@ export const Service = (props) => {
   return (
    <div className="container" onClick={() => onClick(service)}>
       <span className="service-name">{service.name}</span>
-<<<<<<< Updated upstream
-      <img title={service.text}src={service.icon} width="50px" height="50px" />
-=======
       <img alt={service.name} src={service.icon} width="50px" height="50px" />
->>>>>>> Stashed changes
     </div>
   );
 };
