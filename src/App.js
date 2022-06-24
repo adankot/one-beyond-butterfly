c647b8090a9b5d867f571cbe1356b8849e37143b
Imre Kiss <imre.kiss@jaystack.com>
2022-06-24 12:38:22 +0200

    Fix the service box display issue

--- src/App.js
+++ src/App.js
@@ -3,43 +3,37 @@ import React, { useState, useEffect } from 'react';
 import serviceTypes from './serviceTypes';
 
 function App() {
-	const [task, setTask] = useState({
-		title: "Web portal",
-		credits: 12,
-		services: [],
-		requirements: {
-			security: 3,
-			speed: 3,
-			stability: 3,
-		}
-	});
+  const [task, setTask] = useState({
+    title: 'Web portal',
+    credits: 12,
+    services: [],
+    requirements: {
+      security: 3,
+      speed: 3,
+      stability: 3,
+    },
+  });
 
-	useEffect(() => {
-		console.log('bonus checks')
-	}, [task]);
+  useEffect(() => {
+    console.log('bonus checks');
+  }, [task]);
 
-	return (
-		<div className="App">
-			<body>
-			<div>
-				<div className="box">
-					{task.title}
-				</div>
-				<div className="box">
-					<div>
-						AWS box
-					</div>
-					{Object.keys(serviceTypes.aws).map(key =>
-						<div className="service-box">{key}</div>
-					)}
-				</div>
-				<div className="box">
-					Azure box
-				</div>
-			</div>
-			</body>
-		</div>
-	);
+  return (
+    <div className="App">
+      <div>
+        <div className="box">{task.title}</div>
+        <div className="box">
+          <div>AWS box</div>
+          {Object.keys(serviceTypes.aws).map((key) => (
+            <div key={key} className="service-box">
+              {key}
+            </div>
+          ))}
+        </div>
+        <div className="box">Azure box</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
