c19036923d4968ca2e5915288f60e678b1e12d69
tamasadanko <tamas.adanko@guidesmiths.com>
2022-06-24 11:51:18 +0200

    feat: add a frame

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+      <div>
+        <div className="box">
+          Task
+        </div>
+        <div className="box">
+          AWS box
+        </div>
+        <div className="box">
+          Azure box
+        </div>
+      </div>
+      </body>
     </div>
   );
 }
